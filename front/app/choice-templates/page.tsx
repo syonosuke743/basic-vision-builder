75be92d5785e0ca06126c57cd2d52f7e0a714b77
syonosuke743 <syonosuke1996@outlook.jp>
2024-05-08 05:38:18 +0900

    テンプレート選択画面の追加

--- front/app/choice-templates/page.tsx
+++ front/app/choice-templates/page.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import styles from "@/styles/choice-templates.module.css"
 
 const page = () => {
   return (
     <main>
-      choice-templates
+      <h1 className={styles.template_choice}>テンプレート選択</h1>
+        <div className={styles.container}>
+          <h2>シックスハット法</h2>
+          <h2>マンダラチャート</h2>
+          <h2>6W2H法</h2>
+        </div>
     </main>
   )
 }
