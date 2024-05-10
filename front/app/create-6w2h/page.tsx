9fa11d0e73e8ded3fe9301864380a26fab749fd2
syonosuke743 <syonosuke1996@outlook.jp>
2024-05-10 13:13:25 +0900

    テンプレート選択画面作成とログイン画面の作成

---
+++ front/app/create-6w2h/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <h1>6w2h法</h1>
+  )
+}
+
+export default page
--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,9 +1,17 @@
-
+import styles from "@/styles/login.module.css"
+import LoginForm from "@/components/LoginForm";
 
 export default function Home() {
   return (
     <main>
-      login
+      <div className={styles.container}>
+        <div className={styles.description}>
+            <h1>Basic Vision Builderとは</h1>
+            <p>シンプルな操作で、アイデアを思いつくために有用なテンプレートを作成できます。</p>
+            <p>テンプレートを手書きで使いたい場合も、テンプレートを印刷することができます。</p>
+        </div>
+        <LoginForm/>
+    </div>
     </main>
   );
 }
