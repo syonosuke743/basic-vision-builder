aecdef782bc48dd370980abfbaf38eb1182c77dd
syonosuke743 <syonosuke1996@outlook.jp>
2024-09-04 22:37:53 +0900

    nextconfigの設定

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  NEXTAUTH_NGINX_URL: process.env.NEXTAUTH_NGINX_URL,
+  },
+};
 
 export default nextConfig;
